6fd01553b4ea6d14efe86dd566e252b45b8d7664
FranekGlab <79981916+FranekGlab@users.noreply.github.com>
2023-02-19 10:29:19 +0100

    done4

--- lab4/main.js
+++ lab4/main.js
@@ -1,156 +1,125 @@
-const noteTitle = document.getElementById("note-title");
-const noteContent = document.getElementById("note-content");
-const addNoteButton = document.getElementById("add-note");
-const editNoteButton = document.getElementById("edit-note");
-const deleteNoteButton = document.getElementById("delete-note");
-const noteList = document.getElementById("note-list");
+const notes = JSON.parse(localStorage.getItem('notes')) || [];
+const titleInput = document.getElementById('title');
+const contentInput = document.getElementById('content');
+const colorInput = document.getElementById('color');
+console.log(colorInput.value)
+const addBtn = document.getElementById('add-btn');
+const app = document.getElementById('app');
+const pinnedContainer = document.getElementById('pinned');
+let currentIndex = 0;
 
-const notes = JSON.parse(localStorage.getItem("notes")) || [];
+const editForm = document.getElementById('isEdit');
+const editTitleInput = document.getElementById('editTitle');
+const editContentInput = document.getElementById('editContent');
+const editColorInput = document.getElementById('editColor');
+const confirmBtn = document.getElementById('confirm-btn');
+const cancelBtn = document.getElementById('cancel-btn');
 
-function renderNotes() {
-  noteList.innerHTML = "";
-  for (const [index, { title }] of notes.entries()) {
-    const li = document.createElement("li");
-    li.textContent = title;
-    li.setAttribute("data-note-id", index);
-    li.addEventListener("click", () => onNoteClick(index));
-    noteList.appendChild(li);
-  }
-}
+const addForm = document.getElementById('addForm');
 
-function onNoteClick(noteId) {
-  const { title, content } = notes[noteId];
-  noteTitle.value = title;
-  noteContent.value = content;
-  addNoteButton.disabled = true;
-  editNoteButton.disabled = false;
-  deleteNoteButton.disabled = false;
-  removeEventListeners();
-  addNoteButton.addEventListener("click", saveNote);
-  editNoteButton.addEventListener("click", () => saveNote(noteId));
-  deleteNoteButton.addEventListener("click", () => deleteNote(noteId));
-}
+const createNote = (title, content, color, date) => {
+  return { title, content, color, date, isPinned: false};
+};
 
-function addNote() {
-  const note = {
-    title: noteTitle.value,
-    content: noteContent.value,
-  };
+const saveNote = (note) => {
   notes.push(note);
-  localStorage.setItem("notes", JSON.stringify(notes));
+  localStorage.setItem('notes', JSON.stringify(notes));
   renderNotes();
-  clearForm();
-}
+};
 
-function editNote(noteId) {
-  const note = {
-    title: noteTitle.value,
-    content: noteContent.value,
-  };
-  notes[noteId] = note;
-  localStorage.setItem("notes", JSON.stringify(notes));
+const deleteNote = (index) => {
+  notes.splice(index, 1);
+  localStorage.setItem('notes', JSON.stringify(notes));
   renderNotes();
-  clearForm();
-}
+};
 
-function deleteNote(noteId) {
-  notes.splice(noteId, 1);
-  localStorage.setItem("notes", JSON.stringify(notes));
-  renderNotes();
-  clearForm();
-}
-
-function saveNote(noteId = null) {
-  if (editNoteButton.disabled) {
-    addNote();
+const pinNote = (note, noteElement) => {
+  let clone = noteElement.cloneNode()
+  if(note.isPinned == false)
+  {
+    noteElement.remove();
+    pinnedContainer.appendChild(clone)
+    note.isPinned = true
+    location.reload();
   } else {
-    editNote(noteId);
+    noteElement.remove();
+    app.appendChild(clone)
+    note.isPinned = false;
+    location.reload();
   }
+  localStorage.setItem('notes', JSON.stringify(notes));  
 }
 
-function clearForm() {
-  noteTitle.value = "";
-  noteContent.value = "";
-  addNoteButton.disabled = false;
-  editNoteButton.disabled = true;
-  deleteNoteButton.disabled = true;
-  removeEventListeners();
-  addNoteButton.addEventListener("click", addNote);
+const editNote = (index) => {
+    app.style.display = "none";
+    addForm.style.display = "none";
+    editForm.style.display = "flex";
+
+    editTitleInput.value = notes[index].title;
+    editContentInput.value = notes[index].content;
+    editColorInput.value = notes[index].color;
+    currentIndex = index;
+    console.log(currentIndex)
 }
 
-function removeEventListeners() {
-  addNoteButton.removeEventListener("click", saveNote);
-  editNoteButton.removeEventListener("click", saveNote);
-  deleteNoteButton.removeEventListener("click", deleteNote);
+const edit = (index) => {
+    console.log(index)
+    app.style.display = "column";
+    addForm.style.display = "flex";
+    editForm.style.display = "none";
+    let dateTime = new Date();
+
+    notes[index].title = editTitleInput.value;
+    notes[index].content =  editContentInput.value;
+    notes[index].color = editColorInput.value;
+    notes[index].date = dateTime.toLocaleDateString() + " " + dateTime.toLocaleTimeString();
+    localStorage.setItem('notes', JSON.stringify(notes));
+    renderNotes();
 }
 
-function initializeEventListeners() {
-  addNoteButton.addEventListener("click", addNote);
-  editNoteButton.addEventListener("function onNoteClick(event)") 
-    const noteId = event.target.getAttribute("data-note-id");
-    const note = notes[noteId];
-    noteTitle.value = note.title;
-    noteContent.value = note.content;
-    addNoteButton.disabled = true;
-    editNoteButton.disabled = false;
-    deleteNoteButton.disabled = false;
-    addNoteButton.removeEventListener("click", addNote);
-    editNoteButton.removeEventListener("click", editNote);
-    deleteNoteButton.removeEventListener("click", deleteNote);
-    addNoteButton.addEventListener("click", saveNote);
-    editNoteButton.addEventListener("click", saveNote.bind(null, noteId));
-    deleteNoteButton.addEventListener("click", deleteNote.bind(null, noteId));
-    }
-    
-    function addNote() {
-    const note = {
-    title: noteTitle.value,
-    content: noteContent.value,
-    };
-    notes.push(note);
-    localStorage.setItem("notes", JSON.stringify(notes));
-    renderNotes();
-    clearForm();
-    }
-    
-    function editNote(noteId) {
-    const note = {
-    title: noteTitle.value,
-    content: noteContent.value,
-    };
-    notes[noteId] = note;
-    localStorage.setItem("notes", JSON.stringify(notes));
-    renderNotes();
-    clearForm();
-    }
-    
-    function deleteNote(noteId) {
-    notes.splice(noteId, 1);
-    localStorage.setItem("notes", JSON.stringify(notes));
-    renderNotes();
-    clearForm();
-    }
-    
-    function saveNote() {
-    if (editNoteButton.disabled) {
-    addNote();
-    } else {
-    const noteId = parseInt(editNoteButton.getAttribute("data-note-id"));
-    editNote(noteId);
-    }
-    }
-    
-    function clearForm() {
-    noteTitle.value = "";
-    noteContent.value = "";
-    addNoteButton.disabled = false;
-    editNoteButton.disabled = true;
-    deleteNoteButton.disabled = true;
-    addNoteButton.removeEventListener("click", saveNote);
-    editNoteButton.removeEventListener("click", saveNote);
-    deleteNoteButton.removeEventListener("click", deleteNote);
-    addNoteButton.addEventListener("click", addNote);
-    }
-    
-    renderNotes();
-    clearForm();
+const renderNotes = () => {
+    app.innerHTML = '';
+    notes.forEach((note, index) => {
+        const noteEl = document.createElement('div');
+        noteEl.style.backgroundColor = note.color;
+        noteEl.innerHTML = `
+        <h2>${note.title}</h2>
+        <p>${note.content}</p>
+        <p>Date: ${note.date}</p>
+        <button onclick="deleteNote(${index})">Delete Note</button>
+        <button onclick="editNote(${index})">Edit Note</button>
+        `;
+        let pinBtn = document.createElement('button');
+        pinBtn.textContent = 'Pin';
+        pinBtn.addEventListener('click', (e) => {
+          pinNote(note, noteEl)
+        
+        });
+        noteEl.appendChild(pinBtn);
+        console.log(note.color);
+        if(note.isPinned)
+        {
+          pinnedContainer.appendChild(noteEl);
+        } else {        
+          app.appendChild(noteEl);
+        }
+        console.log(app)
+    });
+};
+confirmBtn.addEventListener('click', (e) => {
+    edit(currentIndex);
+})
+
+addBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let dateTime = new Date();
+    const title = titleInput.value;
+    const content = contentInput.value;
+    const color = colorInput.value;
+    const date = dateTime.toLocaleDateString() + " " + dateTime.toLocaleTimeString();
+    saveNote(createNote(title, content, color, date));
+
+});
+
+
+renderNotes();
